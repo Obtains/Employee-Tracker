2f633518a9f1a78e5b18312e18fa399c6b820e6a
Obtains <97128583+Obtains@users.noreply.github.com>
2022-05-23 12:57:51 -0400

    added seeds and schema

---
+++ db/seed.sql
@@ -0,0 +1,32 @@
+INSERT INTO deparment (name)
+VALUES
+('Sales'),
+('IT'),
+('Fiance'),
+('Marketing'),
+('Design');
+
+SELECT * FROM DEPARMENT;
+
+INSERT INTO role (title, salary, deparment_id)
+VALUES
+('Sales Represenative', 70000, 1),
+('IT Specialist', 100000, 2),
+('Accountant Manager', 95000, 3),
+('Accountant', 80000, 3),
+('Product Manager', 95000, 4),
+('Marketing Lead', 85000, 4),
+('Web Design Manager', 120000, 5),
+('Web Developer', 80000, 5);
+
+SELECT * FROM ROLE;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Roronoa', 'Zoro', 3, 1),
+('Monkey D.', 'Luffy', 5, 2),
+('Nami', 'Bellmere', 4, 2),
+('Nico', 'Robin', 2, NULL),
+('Cutty', 'Flam', 1, NULL);
+
+SELECT * FROM employee;
